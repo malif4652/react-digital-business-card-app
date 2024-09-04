6a192660135ce16158a0ef78e32fcca385f8a845
Md. Alif Dewan <malifd470@gmail.com>
2024-09-05 02:11:22 +0600

    little fix

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,11 +1,9 @@
+import profile from "./assets/img/profile.jpg";
+
 export default function Profile() {
   return (
     <div className="profile">
-      <img
-        className="profile--image"
-        src="src/assets/img/profile.jpg"
-        alt="profile"
-      />
+      <img className="profile--image" src={profile} alt="profile" />
       <h1>Md. Alif Dewan</h1>
       <h3>Frontend Developer</h3>
       <p>malifd.website</p>
